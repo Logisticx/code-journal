1984eca8af0ed3c907d4c177f3c21211c7ea1e2f
Andre Le <logisticx@yahoo.com>
2022-05-31 19:49:13 +0000

    completed issue 1 user create entry

--- js/data.js
+++ js/data.js
@@ -1,8 +1,16 @@
 /* exported data */
-
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+window.addEventListener('beforeunload', windowString);
+function windowString(event) {
+  var entriesJSON = JSON.stringify(data.entries);
+  localStorage.setItem('javascript-local-storage', entriesJSON);
+}
+var previousentriesJSON = localStorage.getItem('javascript-local-storage');
+if (previousentriesJSON !== null) {
+  data.entries = JSON.parse(previousentriesJSON);
+}
