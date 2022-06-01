b772360e82da0f4e52902010d7cf4575c53c89ee
Andre Le <logisticx@yahoo.com>
2022-06-01 01:01:12 +0000

    updated JS, fixed styling

--- js/main.js
+++ js/main.js
@@ -21,3 +21,43 @@ function submitEvent(event) {
   document.querySelector('.image').src = 'images/placeholder-image-square.jpg';
   document.querySelector('form').reset();
 }
+
+function domTree(object) {
+  var openingLi = document.createElement('li');
+  var rowDiv = document.createElement('div');
+  rowDiv.className = 'row';
+  openingLi.appendChild(rowDiv);
+  var columnHalfDiv = document.createElement('div');
+  columnHalfDiv.className = 'column-half';
+  rowDiv.appendChild(columnHalfDiv);
+  var divImg = document.createElement('div');
+  divImg.className = 'img';
+  columnHalfDiv.appendChild(divImg);
+  var newImage = document.createElement('img');
+  newImage.className = 'image';
+  newImage.src = (object.img);
+  divImg.appendChild(newImage);
+  var columnHalfDivTwo = document.createElement('div');
+  columnHalfDivTwo.className = 'column-half';
+  rowDiv.appendChild(columnHalfDivTwo);
+  var entriesDiv = document.createElement('div');
+  entriesDiv.className = 'entries-text';
+  columnHalfDivTwo.appendChild(entriesDiv);
+  var hTwo = document.createElement('h2');
+  hTwo.textContent = (object.title);
+  entriesDiv.appendChild(hTwo);
+  var pText = document.createElement('p');
+  pText.textContent = (object.notes);
+  entriesDiv.appendChild(pText);
+
+  return openingLi;
+
+}
+var domContent = document.querySelector('.list');
+document.addEventListener('DOMContentLoaded', domTree);
+for (var i = 0; i < data.entries.length; i++) {
+  var dataEntries = domTree(data.entries[i]);
+
+  domContent.appendChild(dataEntries);
+
+}
