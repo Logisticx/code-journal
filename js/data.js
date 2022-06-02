7984d122d575c90c3296ba1f4c521e95ef22258b
Andre Le <logisticx@yahoo.com>
2022-06-02 06:23:27 +0000

    completed issue-2-user-view-entry

--- js/data.js
+++ js/data.js
@@ -5,6 +5,7 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
 window.addEventListener('beforeunload', windowString);
 function windowString(event) {
   var entriesJSON = JSON.stringify(data.entries);
@@ -15,65 +16,4 @@ function windowString(event) {
 var previousentriesJSON = localStorage.getItem('javascript-local-storage');
 if (previousentriesJSON !== null) {
   data.entries = JSON.parse(previousentriesJSON);
-
 }
-
-if (localStorage.getItem('view') !== 'entries') {
-  data.view = localStorage.getItem('view');
-}
-
-var dataValue = document.querySelectorAll('[data-view]');
-var activeView = document.querySelectorAll('.view');
-var hiddenView = document.querySelectorAll('.hidden');
-var entrySwitch = document.querySelector('.entries-button');
-entrySwitch.addEventListener('click', viewSwitchFunction);
-
-function viewSwitchFunction(event) {
-  for (var i = 0; i <= dataValue.length; i++) {
-    if (dataValue[i] !== activeView) {
-      activeView[i].className = 'hidden';
-      hiddenView[i].className = 'view';
-      activeView[i].setAttribute('data-view', 'entries');
-      hiddenView[i].setAttribute('data-view', 'entry-form');
-      data.view = 'entries';
-      localStorage.setItem('view', data.view);
-    }
-  }
-}
-
-var entriesSwitch = document.querySelector('.new-button');
-entriesSwitch.addEventListener('click', viewSwitchFunctionTwo);
-
-function viewSwitchFunctionTwo(event) {
-  for (var i = 0; i <= dataValue.length; i++) {
-    if (dataValue[i] !== activeView) {
-      activeView[i].className = 'view';
-      hiddenView[i].className = 'hidden';
-      activeView[i].setAttribute('data-view', 'entry-form');
-      hiddenView[i].setAttribute('data-view', 'entries');
-      data.view = 'entry-form';
-      localStorage.setItem('view', data.view);
-    }
-  }
-}
-
-var submitSwitch = document.querySelector('.submit');
-submitSwitch.addEventListener('click', viewSwitchFunctionThree);
-
-function viewSwitchFunctionThree(event) {
-  for (var i = 0; i <= dataValue.length; i++) {
-    if (dataValue[i] !== activeView) {
-      activeView[i].className = 'hidden';
-      hiddenView[i].className = 'view';
-      activeView[i].setAttribute('data-view', 'entries');
-      hiddenView[i].setAttribute('data-view', 'entry-form');
-      data.view = 'entries';
-      localStorage.setItem('view', data.view);
-    }
-  }
-}
-
-/* var dataViewValue = JSON.stringify(data.view);
-localStorage.setItem('data-view', dataViewValue);
-var previousViewsJSON = localStorage.getItem('data-view');
-data.view = JSON.parse(previousViewsJSON); */
--- js/main.js
+++ js/main.js
@@ -6,23 +6,69 @@ function photoFunction(event) {
   document.querySelector('.image').src = imageInput;
 }
 
-var journalEntry = document.querySelector('form');
-journalEntry.addEventListener('submit', submitEvent);
+var noEntriesview = document.querySelector('.no-entries');
+var dataValue = document.querySelectorAll('[data-view]');
+var activeView = document.querySelectorAll('.view');
+var hiddenView = document.querySelectorAll('.hidden');
 
-function submitEvent(event) {
-  var newObj = {};
-  newObj.title = document.querySelector('#title-name').value;
-  newObj.img = document.querySelector('#photo-url').value;
-  newObj.notes = document.querySelector('#notes').value;
-  newObj.currentEntryId = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.unshift(newObj);
-  event.preventDefault();
-  document.querySelector('.image').src = 'images/placeholder-image-square.jpg';
-  document.querySelector('form').reset();
+for (var i = 0; i <= dataValue.length; i++) {
+  if (localStorage.getItem('view') === 'entries') {
+    data.view = localStorage.getItem('view');
+    activeView[0].className = 'hidden';
+    hiddenView[0].className = 'view';
+  }
 }
 
-function domTree(object) {
+var entrySwitch = document.querySelector('.entries-button');
+entrySwitch.addEventListener('click', viewSwitchFunction);
+
+function viewSwitchFunction(event) {
+  for (var i = 0; i <= dataValue.length; i++) {
+    if (dataValue[i] === activeView[i]) {
+      activeView[i].className = 'hidden';
+      activeView[i].setAttribute('data-view', 'entries');
+      data.view = 'entries';
+      localStorage.setItem('view', data.view);
+    } else {
+      hiddenView[0].className = 'view';
+      hiddenView[0].setAttribute('data-view', 'entry-form');
+    }
+  }
+}
+
+var entriesSwitch = document.querySelector('.new-button');
+entriesSwitch.addEventListener('click', viewSwitchFunctionTwo);
+
+function viewSwitchFunctionTwo(event) {
+  for (var i = 0; i <= dataValue.length; i++) {
+    if (dataValue[i] !== activeView) {
+      activeView[i].className = 'view';
+      hiddenView[i].className = 'view hidden';
+      activeView[i].setAttribute('data-view', 'entry-form');
+      hiddenView[i].setAttribute('data-view', 'entries');
+      data.view = 'entry-form';
+      localStorage.setItem('view', data.view);
+    }
+  }
+}
+
+var submitSwitch = document.querySelector('.submit');
+submitSwitch.addEventListener('click', viewSwitchFunctionThree);
+
+function viewSwitchFunctionThree(event) {
+  for (var i = 0; i <= dataValue.length; i++) {
+    if (dataValue[i] !== activeView) {
+      activeView[i].className = 'view hidden';
+      hiddenView[i].className = 'view';
+      activeView[i].setAttribute('data-view', 'entries');
+      hiddenView[i].setAttribute('data-view', 'entry-form');
+      data.view = 'entries';
+      localStorage.setItem('view', data.view);
+    }
+  }
+}
+
+function createEntryElement(object) {
   var openingLi = document.createElement('li');
   var rowDiv = document.createElement('div');
   rowDiv.className = 'row';
@@ -49,47 +95,37 @@ function domTree(object) {
   var pText = document.createElement('p');
   pText.textContent = (object.notes);
   entriesDiv.appendChild(pText);
-
+  noEntriesview.className = 'hidden';
   return openingLi;
 
 }
 var domContent = document.querySelector('.list');
-document.addEventListener('DOMContentLoaded', domTree);
-for (var i = 0; i < data.entries.length; i++) {
-  var dataEntries = domTree(data.entries[i]);
-
-  domContent.appendChild(dataEntries);
-
+function loadEntries() {
+  domContent.innerHTML = '';
+  for (var i = 0; i < data.entries.length; i++) {
+    var dataEntries = createEntryElement(data.entries[i]);
+    domContent.appendChild(dataEntries);
+  }
 }
-/*
-var entrySwitch = document.querySelector('.entries-button');
-entrySwitch.addEventListener('click', viewSwitchFunction);
+document.addEventListener('DOMContentLoaded', loadEntries);
+var journalEntry = document.querySelector('form');
+journalEntry.addEventListener('submit', submitEvent);
 
-var divClassName = document.querySelector('.active');
-var divClassNametwo = document.querySelector('.hidden');
-
-function viewSwitchFunction(event) {
-
-  divClassNametwo.className = 'active';
-  divClassName.className = 'hidden';
-
+function prependEntry(entry) {
+  var entryElement = createEntryElement(entry);
+  domContent.prepend(entryElement);
 }
 
-var entriesSwitch = document.querySelector('.new-button');
-entriesSwitch.addEventListener('click', viewSwitchFunctionTwo);
-
-function viewSwitchFunctionTwo(event) {
-
-  divClassName.className = 'active';
-  divClassNametwo.className = 'hidden';
-
+function submitEvent(event) {
+  var newObj = {};
+  newObj.title = document.querySelector('#title-name').value;
+  newObj.img = document.querySelector('#photo-url').value;
+  newObj.notes = document.querySelector('#notes').value;
+  newObj.currentEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(newObj);
+  event.preventDefault();
+  document.querySelector('.image').src = 'images/placeholder-image-square.jpg';
+  document.querySelector('form').reset();
+  prependEntry(newObj);
 }
-
-var submitSwitch = document.querySelector('.submit');
-submitSwitch.addEventListener('click', viewSwitchFunctionThree);
-
-function viewSwitchFunctionThree(event) {
-  divClassName.className = 'hidden';
-  divClassNametwo.className = 'active';
-}
-*/
